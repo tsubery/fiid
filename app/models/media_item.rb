01dc7a47e387f0247400fbcd7fdc4dcec8172357
Gal Tsubery <gal.tsubery@gmail.com>
2024-06-16 13:53:04 -0500

    Don't assume feed exist (would fail later)

--- app/models/media_item.rb
+++ app/models/media_item.rb
@@ -45,7 +45,7 @@ class MediaItem < ApplicationRecord
           self.reachable = false
         end
         self.author = info["uploader"] || ''
-        self.title = [feed.title, info["title"]].select(&:present?).join(" - ")
+        self.title = [feed&.title, info["title"]].select(&:present?).join(" - ")
         self.published_at = info["upload_date"] && Date.parse(info["upload_date"])
         self.description = "Original Video: #{url}\nPublished At: #{published_at}\n #{info["description"]}"
         self.duration_seconds = info["duration"]
